f159e10e542278ae41e32bd0591dea2a2476136a
sei233 <1136420833@qq.com>
2018-07-18 15:37:23 +0800

    提交时间:2018/7/18 15:36 提交人：石乾 提交内容：spring+mybatis框架

---
+++ src/com/peanut/bean/po/Game.java
@@ -0,0 +1,85 @@
+package com.peanut.bean.po;
+
+public class Game {
+    private Integer id;
+    private String name;
+    private String title;
+    private Integer download;
+    private Integer size;
+    private Boolean status;
+    private String type;
+    private String operation;
+    private String advise_type;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getDownload() {
+        return download;
+    }
+
+    public void setDownload(Integer download) {
+        this.download = download;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public String getAdvise_type() {
+        return advise_type;
+    }
+
+    public void setAdvise_type(String advise_type) {
+        this.advise_type = advise_type;
+    }
+}
